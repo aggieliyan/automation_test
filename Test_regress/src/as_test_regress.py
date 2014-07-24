66f99a70e306074dc782122c9b014b9e147e042b
yilu282 <1049924595@qq.com>
2014-07-24 18:01:16 +0800

    处理冲突

--- Test_regress/src/as_test_regress.py
+++ Test_regress/src/as_test_regress.py
@@ -802,21 +802,14 @@ class Test(unittest.TestCase):
         #self.use_catecard()       
         #self.buy_course_use_RMB()
         #self.buy_course_use_card()
-<<<<<<< HEAD
-        #self.createpaper() 
+        self.createpaper() 
         #exam_paper.exam_result(self.cfg, self.driver, self.base_url, exam_name=u"未作答（主观题，免费）", etype=1)
         #exam_paper.exam_result(self.cfg, self.driver, self.base_url, exam_name=u"未作答（主观题，免费）", etype=2)
         #exam_paper.exam_result(self.cfg, self.driver, self.base_url, exam_name=u"未作答（主观题，免费）", etype=3)
         #exam_paper.send_close_paper(self.cfg, self.driver, self.base_url, atype=1)
         #exam_paper.send_close_paper(self.cfg, self.driver, self.base_url, atype=2)
-        exam_user_management.buy_paper(self.cfg, self.driver, self.base_url)
-        
-=======
-        self.createpaper() 
-        #exam_paper.exam_result(self.cfg, self.driver, self.base_url, exam_name=u"未作答（主观题，免费）")
-        #exam_paper.send_close_paper(self.cfg, self.driver, self.base_url)
         #exam_user_management.buy_paper(self.cfg, self.driver, self.base_url)
->>>>>>> origin/master
+        
        
     def tearDown(self):
         self.driver.quit()

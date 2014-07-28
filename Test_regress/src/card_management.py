452990c32ba9644cce5a12a055bdc5ac1816693e
gaoyue001 <775694463@qq.com>
2014-07-28 18:46:05 +0800

    修改考试卡

--- Test_regress/src/card_management.py
+++ Test_regress/src/card_management.py
@@ -145,8 +145,43 @@ def add_card(cfg, driver, base_url, org_name, cgroup_num = 1,card_prifix='auto',
     driver.find_element(cfg.get('org_manage','add_card_ok_css_by'),cfg.get('org_manage','add_card_ok_css')).click()
     time.sleep(2)
 
-#添加考试卡
-def add_exam_card(cfg, driver, base_url,count = 5):
+#机构获取院校机构类目名称
+def get_academy_catename(cfg, driver, base_url):
+    time.sleep(2)
+    driver.get(base_url + "qqhru")
+    time.sleep(2)
+    driver.find_element_by_link_text(u"跳过").click()#点击跳过
+    time.sleep(2)
+    driver.find_element(cfg.get('org_manage','exam_selectcourse_by'),cfg.get('org_manage','exam_selectcourse')).click()#点击选课
+    time.sleep(2)
+    academy_catename = driver.execute_script("return $(\'.wrap span\').eq(0).text()")#获取第一个课程名称
+    time.sleep(2) 
+    driver.find_element(cfg.get('org_manage','exam_select_close_by'),cfg.get('org_manage','exam_select_close')).click()#关闭窗口   
+    time.sleep(2)
+    return academy_catename
+#创建考试卡获取第一个考号
+def add_exam_card(cfg, driver, base_url,count):
+    time.sleep(2)
+    driver.execute_script("$('#paper_list_con a:eq(6)').click()")#点击更多
+    time.sleep(2)
+    driver.execute_script("$('#paper_list_con li:eq(0)').click()")#点击分配试卷  
+    time.sleep(2)
+    driver.find_element(cfg.get('org_manage','exam_subname_by'),cfg.get('org_manage','exam_subname')).send_keys("qqhru")#添加机构名称 
+    time.sleep(2)
+    driver.find_element(cfg.get('org_manage','exam_cardcount_by'),cfg.get('org_manage','exam_cardcount')).send_keys(count)#填写考试卡数量
+    time.sleep(2)
+    driver.find_element(cfg.get('org_manage','exam_ok_by'),cfg.get('org_manage','exam_ok')).click()#点击确定按钮
+    time.sleep(2)
+    driver.execute_script("$('#paper_list_con a:eq(6)').click()")#点击更多
+    lookcardnum_url = driver.execute_script("return $('#paper_list_con li:eq(1)').attr('data-url')")#获取点击查看卡号触发的链接
+    time.sleep(2)
+    driver.get(lookcardnum_url)#定位查看卡号连接
+    time.sleep(2)
+    examcard_number =driver.execute_script("return $('.first-cell span:eq(1)').text()")
+    time.sleep(2)
+    return examcard_number
+#获取院校机构课程类目名并创建试卷后，创建卡号，并返回    
+def add_exam_card1(cfg, driver, base_url,count = 5):
     time.sleep(2)
     driver.get(base_url + "qqhru")
     time.sleep(2)

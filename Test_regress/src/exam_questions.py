a3c78e5775a3d67c39d92244a92c66c380fce53f
hjliu <hjliu@ablesky.com>
2014-07-30 15:45:08 +0800

    格式化

--- Test_regress/src/exam_questions.py
+++ Test_regress/src/exam_questions.py
@@ -46,11 +46,13 @@ def exam_questions(cfg,driver, base_url,question_ansa):
     driver.find_element_by_link_text("单选题").click()
     time.sleep(2) 
     #创建单选题   
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     #添加音频
     #driver.find_element_by_xpath("//html/body/div[2]/div[3]/div/div/div[2]/div/dl[3]/dd/div/p[1]/a").click()
@@ -59,141 +61,185 @@ def exam_questions(cfg,driver, base_url,question_ansa):
     
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(2)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(3)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_by"),\
+        cfg.get('exam_questions',"question_save")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
     time.sleep(2)
     
     #创建多选题   
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_by"),cfg.get('exam_questions',"question_type")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_by"),\
+        cfg.get('exam_questions',"question_type")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_duoxuan_by"),cfg.get('exam_questions',"question_type_duoxuan")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_duoxuan_by"),\
+        cfg.get('exam_questions',"question_type_duoxuan")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(2)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(3)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_by"),\
+        cfg.get('exam_questions',"question_save")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
     time.sleep(2)
     
     #创建是非题
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_by"),cfg.get('exam_questions',"question_type")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_by"),\
+        cfg.get('exam_questions',"question_type")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_shifei_by"),cfg.get('exam_questions',"question_type_shifei")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_shifei_by"),\
+        cfg.get('exam_questions',"question_type_shifei")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_by"),\
+        cfg.get('exam_questions',"question_save")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
 #    time.sleep(2)
     
     #创建问答题
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_by"),cfg.get('exam_questions',"question_type")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_by"),\
+        cfg.get('exam_questions',"question_type")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_wenda_by"),cfg.get('exam_questions',"question_type_wenda")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_wenda_by"),\
+        cfg.get('exam_questions',"question_type_wenda")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(2)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_by"),\
+        cfg.get('exam_questions',"question_save")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
     
     #创建填空题
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_by"),cfg.get('exam_questions',"question_type")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_by"),\
+        cfg.get('exam_questions',"question_type")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_tiankong_by"),cfg.get('exam_questions',"question_type_tiankong")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_tiankong_by"),\
+        cfg.get('exam_questions',"question_type_tiankong")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     #driver.find_element_by_xpath(cfg.get('exam_questions','question_tiankonganswer_xpath_by')).send_keys("123")
-    driver.find_element(cfg.get('exam_questions',"question_tiankonganswer_by"),cfg.get('exam_questions',"question_tiankonganswer")).send_keys(question_ansa)
+    driver.find_element(cfg.get('exam_questions',"question_tiankonganswer_by"),\
+        cfg.get('exam_questions',"question_tiankonganswer")).send_keys(question_ansa)
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_by"),\
+        cfg.get('exam_questions',"question_save")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
     
     #创建完型填空题
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_by"),cfg.get('exam_questions',"question_type")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_by"),\
+        cfg.get('exam_questions',"question_type")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_wanxing_by"),cfg.get('exam_questions',"question_type_wanxing")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_wanxing_by"),\
+        cfg.get('exam_questions',"question_type_wanxing")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer1_by"),cfg.get('exam_questions',"question_wanxinganswer1")).send_keys(question_ansa)
+    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer1_by"),\
+        cfg.get('exam_questions',"question_wanxinganswer1")).send_keys(question_ansa)
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer2_by"),cfg.get('exam_questions',"question_wanxinganswer2")).send_keys(question_ansa)
+    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer2_by"),\
+        cfg.get('exam_questions',"question_wanxinganswer2")).send_keys(question_ansa)
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer3_by"),cfg.get('exam_questions',"question_wanxinganswer3")).send_keys(question_ansa)
+    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer3_by"),\
+        cfg.get('exam_questions',"question_wanxinganswer3")).send_keys(question_ansa)
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer4_by"),cfg.get('exam_questions',"question_wanxinganswer4")).send_keys(question_ansa)
+    driver.find_element(cfg.get('exam_questions',"question_wanxinganswer4_by"),\
+        cfg.get('exam_questions',"question_wanxinganswer4")).send_keys(question_ansa)
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_by"),\
+        cfg.get('exam_questions',"question_save")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
     
     #创建综合题
-    driver.find_element(cfg.get('exam_questions',"question_creat_by"),cfg.get('exam_questions',"question_creat")).click()
+    driver.find_element(cfg.get('exam_questions',"question_creat_by"),\
+        cfg.get('exam_questions',"question_creat")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_by"),cfg.get('exam_questions',"question_type")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_by"),\
+        cfg.get('exam_questions',"question_type")).click()
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_type_zonghe_by"),cfg.get('exam_questions',"question_type_zonghe")).click()
+    driver.find_element(cfg.get('exam_questions',"question_type_zonghe_by"),\
+        cfg.get('exam_questions',"question_type_zonghe")).click()
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(0)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(2)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(4)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
     iframe_id = driver.execute_script("return $('#sbjFormCon iframe:eq(5)').attr('id')")    
     driver.execute_script("var element=window.document.getElementById(\'" + iframe_id + "\');\
-    idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML =\'" + question_ansa + "\';")
+    idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
+    element.innerHTML =\'" + question_ansa + "\';")
     time.sleep(2)
-    driver.find_element(cfg.get('exam_questions',"question_save_by"),cfg.get('exam_questions',"question_save")).click()
+    driver.find_element(cfg.get('exam_questions',"question_save_id_by"),\
+        cfg.get('exam_questions',"question_save_id")).click()
     time.sleep(2)
     driver.find_element_by_link_text("单选题").click()
     time.sleep(2)

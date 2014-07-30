a2bb4fcec557dd38d834324a84fa0c0d53ffdc24
yilu282 <1049924595@qq.com>
2014-07-30 09:49:13 +0800

    测试文件

--- Test_regress/src/test_create_student.py
+++ Test_regress/src/test_create_student.py
@@ -6,6 +6,7 @@ Created on Oct 23, 2012
 import student_management,login,ConfigParser
 from selenium import webdriver
 import os
+import admin_management
 
 def test_create_student():
     
@@ -19,14 +20,19 @@ def test_create_student():
     #driver.implicitly_wait(30)
     user_name = "zhongyan"
     user_psw = "1234"
+    org_name = user_name
     for i in range(1):
         print 'wwwwwwwwwwwwwwwwwwwwwww'
         chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = chromedriver
         driver =  webdriver.Chrome(chromedriver)
         stu_num = 2
-        login.auto_register(cfg, driver, base_url, 2, 1)
-        #login.login_by_logindo(cfg, driver, base_url, user_name, user_psw)
+        login.login_by_logindo(cfg, driver, base_url, user_name, user_psw)
+        #admin_management.auto_create_admin(cfg,driver, base_url, org_name="zhongyan", adm_num=2)
+        admin_management.modify_admin(cfg,driver, base_url, org_name)
+        admin_management.delete_admin(cfg,driver, base_url, org_name, admin_num=1)
+        time.sleep(10)
+        #login.auto_register(cfg, driver, base_url, 2, 1)
         #student_management.auto_create_student(cfg, driver, base_url, user_name, stu_num)
         driver.quit()
     

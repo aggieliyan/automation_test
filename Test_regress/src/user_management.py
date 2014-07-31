ce0ce50421b5c4e8af6a8325668e13cc66a90714
fanqiao <541791463@qq.com>
2014-07-31 20:06:39 +0800

    更新
    更新

--- Test_regress/src/user_management.py
+++ Test_regress/src/user_management.py
@@ -14,7 +14,7 @@ def buy_course(cfg, driver, base_url, org_name):
     time.sleep(8)
     driver.find_element_by_link_text(u"课程中心").click()
     time.sleep(10)
-    driver.find_element(cfg.get('org_index','wl_href_css_by'),cfg.get('org_index','wl_href_css')).click()
+    driver.find_element(cfg.get('org_index','buy_coursec1_by'),cfg.get('org_index','buy_coursec1')).click()
     time.sleep(8)
     h = driver.window_handles
     driver.switch_to_window(h[-1])
@@ -23,13 +23,13 @@ def buy_course(cfg, driver, base_url, org_name):
     time.sleep(10)
     h = driver.window_handles
     driver.switch_to_window(h[-1])
-    driver.find_element(cfg.get('org_index','wl_buycou_id_by'),cfg.get('org_index','wl_buycou_id')).click()
+    driver.find_element(cfg.get('org_index','buy_coursei_by'),cfg.get('org_index','buy_coursei')).click()
     time.sleep(12)
-    driver.find_element(cfg.get('org_index','wl_buycou_css_by'),cfg.get('org_index','wl_buycou_css')).click()
+    driver.find_element(cfg.get('org_index','buy_coursec2_by'),cfg.get('org_index','buy_coursec2')).click()
     time.sleep(8)
-    driver.find_element(cfg.get('org_index','wl_buycou_css_by'),cfg.get('org_index','wl_buycou_css')).click()
+    driver.find_element(cfg.get('org_index','buy_coursec2_by'),cfg.get('org_index','buy_coursec2')).click()
     time.sleep(3)
-    driver.find_element(cfg.get('org_index','wl_buycou_css_by'),cfg.get('org_index','wl_buycou_css')).click()
+    driver.find_element(cfg.get('org_index','buy_coursec2_by'),cfg.get('org_index','buy_coursec2')).click()
     time.sleep(3)    
     
 #个人充值卡买课
@@ -39,7 +39,8 @@ def buy_course_usecard(cfg, driver, base_url, org_name):
     time.sleep(8)
     driver.find_element_by_link_text(u"课程中心").click()
     time.sleep(10)
-    driver.find_element(cfg.get('org_index','wl_href_css_by'),cfg.get('org_index','wl_href_css')).click()
+    driver.find_element(cfg.get('org_index','buy_coursec1_by'),cfg.get('org_index','buy_coursec1')).click()
+    #driver.find_element(cfg.get('org_index','wl_href_css_by'),cfg.get('org_index','wl_href_css')).click()
     time.sleep(8)
     h = driver.window_handles
     driver.switch_to_window(h[-1])
@@ -48,7 +49,8 @@ def buy_course_usecard(cfg, driver, base_url, org_name):
     time.sleep(12)
     h = driver.window_handles
     driver.switch_to_window(h[-1])
-    driver.find_element(cfg.get('org_index','wl_buycou_css_by'),cfg.get('org_index','wl_buycou_css')).click()
+    #driver.find_element(cfg.get('org_index','wl_buycou_css_by'),cfg.get('org_index','wl_buycou_css')).click()
+    driver.find_element(cfg.get('org_index','buy_coursec2_by'),cfg.get('org_index','buy_coursec2')).click()
     time.sleep(10)
     
 #个人发照片 数量最大10
@@ -118,15 +120,15 @@ def release_announcement(cfg,driver, base_url, org_name, title, an_content=u'这
     time.sleep(2)
     driver.find_element_by_link_text(u"网校公告").click()
     time.sleep(5)
-    driver.find_element(cfg.get('org_index','ann_notice_xpath_by'),cfg.get('org_index','ann_notice_xpath')).click()
+    driver.find_element(cfg.get('org_index','release_announcementx_by'),cfg.get('org_index','release_announcementx')).click()
     time.sleep(2)
-    driver.find_element(cfg.get('org_index','ann_select_css_by'),cfg.get('org_index','ann_select_css')).send_keys(u"公告")
+    driver.find_element(cfg.get('org_index','release_announcementc_by'),cfg.get('org_index','release_announcementc')).send_keys(u"公告")
     an_content = an_content.replace("\"","\\\"").replace("\'","\\\'")
     time.sleep(2)
     driver.execute_script("var element=window.document.getElementById('editNotice_ifr');\
     idocument=element.contentDocument;element=idocument.getElementById('tinymce');element.innerHTML='" +an_content+ "'")
     time.sleep(5)
-    driver.find_element(cfg.get('org_index','ann_ok_xpath_by'),cfg.get('org_index','ann_ok_xpath')).click()
+    driver.find_element(cfg.get('org_index','release_announcementx2_by'),cfg.get('org_index','release_announcementx2')).click()
     time.sleep(3)
     return title
 
@@ -138,11 +140,10 @@ def release_href_course(cfg, driver, base_url, org_name):
     driver.find_element_by_xpath(u"(//a[contains(text(),'课程中心')])[2]").click()
     time.sleep(8)
     driver.find_element_by_css_selector("div.coursecenter-details-pic > a > img").click()
-    #driver.find_element(cfg.get('org_index','wl_href_css'),cfg.get('org_index','wl_href_css')).click()
     time.sleep(8)
     h = driver.window_handles
     driver.switch_to_window(h[-1])
-    driver.find_element(cfg.get('org_index','wl_ann_id1_by'),cfg.get('org_index','wl_ann_id1')).click() 
+    driver.find_element(cfg.get('org_index','release_hrefi_by'),cfg.get('org_index','release_hrefi')).click() 
     time.sleep(4)
     textarea = driver.execute_script("return $('textarea:eq(2)').text()")
     time.sleep(8)
@@ -177,33 +178,33 @@ def org_chang_headpic(cfg, driver, base_url, org_name, head_pic = r"W:\Testing\T
     
     driver.get(base_url + "myOffice.do")
     time.sleep(5)
-    driver.find_element(cfg.get('org_index','head_picname_name_by'),cfg.get('org_index','head_picname_name')).send_keys(head_pic)
+    driver.find_element(cfg.get('org_index','head_picname_by'),cfg.get('org_index','head_picname')).send_keys(head_pic)
     time.sleep(8)
 #机构首页logo   
 def change_homelogo(cfg, driver, base_url, org_name, logo_pic = r"W:\Testing\Testing Files\Automation_test\headpic.jpg"):
   
     driver.get(base_url + org_name)
     time.sleep(2)
-    driver.find_element(cfg.get('org_index','home_logoname_name_by'),cfg.get('org_index','home_logoname_name')).send_keys(logo_pic)
+    driver.find_element(cfg.get('org_index','home_logoname_by'),cfg.get('org_index','home_logoname')).send_keys(logo_pic)
     time.sleep(8)
 
 #修改页脚
 def modify_pagefoot(cfg,driver, base_url, org_name):
    
     driver.get(base_url + org_name)
-    time.sleep(2)
+    time.sleep(4)
     driver.find_element_by_link_text(u"编辑页脚").click()
     h = driver.window_handles
     driver.switch_to_window(h[-1])
-    driver.find_element(cfg.get('org_index','pf_mod_xpath_by'),cfg.get('org_index','pf_mod_xpath')).click()
+    driver.find_element(cfg.get('org_index','pf_modx_by'),cfg.get('org_index','pf_modx')).click()
+    time.sleep(5)
+    driver.find_element(cfg.get('org_index','pf_modx2_by'),cfg.get('org_index','pf_modx2')).clear()
+    time.sleep(5)
+    driver.find_element(cfg.get('org_index','pf_modx3_by'),cfg.get('org_index','pf_modx3')).send_keys("0")
     time.sleep(2)
-    driver.find_element(cfg.get('org_index','pf_mod2_xpath_by'),cfg.get('org_index','pf_mod2_xpath')).clear()
+    driver.find_element(cfg.get('org_index','pf_modx4_by'),cfg.get('org_index','pf_modx4')).send_keys("sdfsdf")
     time.sleep(2)
-    driver.find_element(cfg.get('org_index','pf_mod3_xpath_by'),cfg.get('org_index','pf_mod3_xpath')).send_keys("0")
-    time.sleep(2)
-    driver.find_element(cfg.get('org_index','pf_mod4_xpath_by'),cfg.get('org_index','pf_mod4_xpath')).send_keys("sdfsdf")
-    time.sleep(2)
-    driver.find_element(cfg.get('org_index','pf_mod5_css_by'),cfg.get('org_index','pf_mod5_css')).click()
+    driver.find_element(cfg.get('org_index','pf_modc5_by'),cfg.get('org_index','pf_modc5')).click()
     time.sleep(4)
 
 

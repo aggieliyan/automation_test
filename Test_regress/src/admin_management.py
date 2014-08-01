68d65ac90208937b017d2be9c8ac978f6da8d21a
yguo <yguo@ablesky.com>
2014-08-01 15:11:03 +0800

    加空格

--- Test_regress/src/admin_management.py
+++ Test_regress/src/admin_management.py
@@ -17,16 +17,16 @@ def create_admin(cfg, driver, base_url, admin_name, admin_username, admin_psw, a
     driver.implicitly_wait(10)
     driver.find_element_by_xpath(cfg.get('org_manage', 'add_admin_xpath')).click()#添加管理员
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('org_manage','ad_name_by'),cfg.get('org_manage','ad_name_id')).send_keys(admin_name)
-    driver.find_element(cfg.get('org_manage','ad_username_by'),cfg.get('org_manage','ad_username_id')).send_keys(admin_username)
-    driver.find_element(cfg.get('org_manage','ad_psw_by'),cfg.get('org_manage','ad_psw_id')).send_keys(admin_psw)
-    driver.find_element(cfg.get('org_manage','ad_repsw_by'),cfg.get('org_manage','ad_repsw_id')).send_keys(admin_psw)
-    driver.find_element(cfg.get('org_manage','ad_email_by'),cfg.get('org_manage','ad_email_id')).send_keys(admin_email)
-    driver.find_element(cfg.get('org_manage','ad_reemail_by'),cfg.get('org_manage','ad_reemail_id')).send_keys(admin_email)
-    driver.find_element(cfg.get('org_manage','per_menber_by'),cfg.get('org_manage','per_menber_id')).click()
-    driver.find_element(cfg.get('org_manage','per_student_by'),cfg.get('org_manage','per_student_id')).click()
-    driver.find_element(cfg.get('org_manage','per_account_by'),cfg.get('org_manage','per_account_id')).click()
-    driver.find_element(cfg.get('org_manage','per_admin_by'),cfg.get('org_manage','per_admin_id')).click()
+    driver.find_element(cfg.get('org_manage', 'ad_name_by'),cfg.get('org_manage', 'ad_name_id')).send_keys(admin_name)
+    driver.find_element(cfg.get('org_manage', 'ad_username_by'),cfg.get('org_manage', 'ad_username_id')).send_keys(admin_username)
+    driver.find_element(cfg.get('org_manage', 'ad_psw_by'),cfg.get('org_manage', 'ad_psw_id')).send_keys(admin_psw)
+    driver.find_element(cfg.get('org_manage', 'ad_repsw_by'),cfg.get('org_manage', 'ad_repsw_id')).send_keys(admin_psw)
+    driver.find_element(cfg.get('org_manage', 'ad_email_by'),cfg.get('org_manage', 'ad_email_id')).send_keys(admin_email)
+    driver.find_element(cfg.get('org_manage', 'ad_reemail_by'),cfg.get('org_manage', 'ad_reemail_id')).send_keys(admin_email)
+    driver.find_element(cfg.get('org_manage', 'per_menber_by'),cfg.get('org_manage', 'per_menber_id')).click()
+    driver.find_element(cfg.get('org_manage', 'per_student_by'),cfg.get('org_manage', 'per_student_id')).click()
+    driver.find_element(cfg.get('org_manage', 'per_account_by'),cfg.get('org_manage', 'per_account_id')).click()
+    driver.find_element(cfg.get('org_manage', 'per_admin_by'),cfg.get('org_manage', 'per_admin_id')).click()
     driver.implicitly_wait(10)
     driver.execute_script("$('.x-btn-text').eq(0).click()")
     driver.implicitly_wait(10)
@@ -43,7 +43,7 @@ def auto_create_admin(cfg, driver, base_url, org_name, adm_num):
         admin_email = admin_name+"@ablesky.com"
         create_admin(cfg,driver, base_url,admin_name,admin_username,admin_psw,admin_email)
         admin_info.append(admin_name)
-   
+
     return admin_info
 
 def delete_admin(cfg, driver, base_url, admin_num=1):
@@ -62,10 +62,9 @@ def delete_admin(cfg, driver, base_url, admin_num=1):
     #else:
     #    driver.find_element_by_xpath("//div["+str(2+admin_num)+"]/div/div/div[2]/div[3]/a").click()
     time.sleep(1) 
-    driver.find_element(cfg.get('org_manage','delete_ad_ok_xpath_by'),cfg.get('org_manage','delete_ad_ok_xpath')).click()
+    driver.find_element(cfg.get('org_manage', 'delete_ad_ok_xpath_by'),cfg.get('org_manage', 'delete_ad_ok_xpath')).click()
     driver.implicitly_wait(10)
     time.sleep(3)
-    
 
 def modify_admin(cfg, driver, base_url):
     driver.get(base_url + "myOffice.do")
@@ -79,8 +78,8 @@ def modify_admin(cfg, driver, base_url):
     #driver.find_element(cfg.get('org_index','org_manage_xpath_by'),cfg.get('org_index','org_manage_xpath')).click()
     driver.implicitly_wait(10)
     driver.find_element_by_link_text(u"编辑管理员").click()   
-    driver.find_element(cfg.get('org_manage','ad_name_by'),cfg.get('org_manage','ad_name_id')).clear()
-    driver.find_element(cfg.get('org_manage','ad_name_by'),cfg.get('org_manage','ad_name_id')).send_keys(admin_name)
+    driver.find_element(cfg.get('org_manage', 'ad_name_by'),cfg.get('org_manage', 'ad_name_id')).clear()
+    driver.find_element(cfg.get('org_manage', 'ad_name_by'),cfg.get('org_manage', 'ad_name_id')).send_keys(admin_name)
     driver.implicitly_wait(10)
     #driver.find_element(cfg.get('org_manage','admin_modify_xpath_by'),cfg.get('org_manage','admin_modify_xpath')).click()
     driver.execute_script("$(\"#editButton button\").eq(0).click()")
@@ -88,4 +87,3 @@ def modify_admin(cfg, driver, base_url):
     return admin_name
     time.sleep(1)
     return admin_name
-    
--- Test_regress/src/exam_cate_management.py
+++ Test_regress/src/exam_cate_management.py
@@ -9,15 +9,15 @@ Created on 2014-7-23
 import unittest, time, re,random
 
 
-    
+
 def create_subject(cfg, driver, base_url, org_name, subject_name):
     driver.get("%sexam/" %(base_url))
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','new_subject_by'),cfg.get('exam','new_subject_id')).click()
-    driver.find_element(cfg.get('exam','sub_name_by'),cfg.get('exam','sub_name')).clear()
-    driver.find_element(cfg.get('exam','sub_name_by'),cfg.get('exam','sub_name')).send_keys(subject_name)
+    driver.find_element(cfg.get('exam', 'new_subject_by'),cfg.get('exam', 'new_subject_id')).click()
+    driver.find_element(cfg.get('exam', 'sub_name_by'),cfg.get('exam', 'sub_name')).clear()
+    driver.find_element(cfg.get('exam', 'sub_name_by'),cfg.get('exam', 'sub_name')).send_keys(subject_name)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','sub_ok_by'),cfg.get('exam','sub_ok_xpath')).click()
+    driver.find_element(cfg.get('exam', 'sub_ok_by'),cfg.get('exam', 'sub_ok_xpath')).click()
     driver.implicitly_wait(10)
 
 def auto_create_subject(cfg, driver, base_url, org_name, sub_num):
@@ -37,15 +37,15 @@ def modify_subject(cfg, driver, base_url, org_name):
     edit_name = driver.execute_script("return $('.subject-item-con').eq(0).children().eq(0).text()")
     subject_name = org_name[0] +"sub_" +prefix
     if edit_name != u'默认科目':        
-        driver.find_element(cfg.get('exam','sub_big1_by'),cfg.get('exam','sub_big1_xpath')).click()
-        driver.find_element(cfg.get('exam','sub_small1_by'),cfg.get('exam','sub_small1_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_big1_by'),cfg.get('exam', 'sub_big1_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_small1_by'),cfg.get('exam', 'sub_small1_xpath')).click()
     else:
         driver.implicitly_wait(10)
-        driver.find_element(cfg.get('exam','sub_big2_by'),cfg.get('exam','sub_big2_xpath')).click()
-        driver.find_element(cfg.get('exam','sub_small2_by'),cfg.get('exam','sub_small2_xpath')).click() 
-    driver.find_element(cfg.get('exam','sub_name_by'),cfg.get('exam','sub_name')).clear()
-    driver.find_element(cfg.get('exam','sub_name_by'),cfg.get('exam','sub_name')).send_keys(subject_name)
-    driver.find_element(cfg.get('exam','sub_ok_by'),cfg.get('exam','sub_ok_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_big2_by'),cfg.get('exam', 'sub_big2_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_small2_by'),cfg.get('exam', 'sub_small2_xpath')).click() 
+    driver.find_element(cfg.get('exam', 'sub_name_by'),cfg.get('exam', 'sub_name')).clear()
+    driver.find_element(cfg.get('exam', 'sub_name_by'),cfg.get('exam', 'sub_name')).send_keys(subject_name)
+    driver.find_element(cfg.get('exam', 'sub_ok_by'),cfg.get('exam', 'sub_ok_xpath')).click()
     driver.implicitly_wait(10)   
     return subject_name
 
@@ -54,31 +54,29 @@ def delete_subject(cfg, driver, base_url, org_name, sub_num=1):
     driver.implicitly_wait(10)
     del_name = driver.execute_script("return $('.subject-item-con').eq(0).children().eq(0).text()")
     if del_name != u'默认科目':        
-        driver.find_element(cfg.get('exam','sub_big1_by'),cfg.get('exam','sub_big1_xpath')).click()
-        driver.find_element(cfg.get('exam','sub_del1_by'),cfg.get('exam','sub_del1_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_big1_by'),cfg.get('exam', 'sub_big1_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_del1_by'),cfg.get('exam', 'sub_del1_xpath')).click()
     else:
         driver.implicitly_wait(10)
-        driver.find_element(cfg.get('exam','sub_big2_by'),cfg.get('exam','sub_big2_xpath')).click()
-        driver.find_element(cfg.get('exam','sub_del2_by'),cfg.get('exam','sub_del2_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_big2_by'),cfg.get('exam', 'sub_big2_xpath')).click()
+        driver.find_element(cfg.get('exam', 'sub_del2_by'),cfg.get('exam', 'sub_del2_xpath')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','sub_delok_by'),cfg.get('exam','sub_delok_xpath')).click()
+    driver.find_element(cfg.get('exam', 'sub_delok_by'),cfg.get('exam', 'sub_delok_xpath')).click()
     driver.implicitly_wait(10)
-    
-    
+
 def create_exam_cate(cfg, driver, base_url, org_name, cate_name, cate_detail):
     driver.get("%sexam/" %(base_url))
     driver.implicitly_wait(10) 
     driver.find_element_by_link_text(u"类目管理").click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','cate_newcateid_by'),cfg.get('exam','cate_newcateid')).click()
+    driver.find_element(cfg.get('exam', 'cate_newcateid_by'),cfg.get('exam', 'cate_newcateid')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','cate_addname_by'),cfg.get('exam','cate_addname')).send_keys(cate_name)
-    driver.find_element(cfg.get('exam','cate_desname_by'),cfg.get('exam','cate_desname')).clear()
-    driver.find_element(cfg.get('exam','cate_desname_by'),cfg.get('exam','cate_desname')).send_keys(cate_detail)
+    driver.find_element(cfg.get('exam', 'cate_addname_by'),cfg.get('exam', 'cate_addname')).send_keys(cate_name)
+    driver.find_element(cfg.get('exam', 'cate_desname_by'),cfg.get('exam', 'cate_desname')).clear()
+    driver.find_element(cfg.get('exam', 'cate_desname_by'),cfg.get('exam', 'cate_desname')).send_keys(cate_detail)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','cate_oknew_button_by'),cfg.get('exam','cate_oknew_button')).click()
+    driver.find_element(cfg.get('exam', 'cate_oknew_button_by'),cfg.get('exam', 'cate_oknew_button')).click()
     driver.implicitly_wait(10)
-    
 
 def auto_create_exam_cate(cfg, driver, base_url, org_name, cate_num):
     
@@ -99,23 +97,23 @@ def modify_exam_cate(cfg, driver, base_url, org_name):
     prefix = chr(random.randint(97,122))+chr(random.randint(97,122))+chr(random.randint(97,122))
     cate_name = org_name[0] +"cate_" +prefix
     cate_detail = org_name[0] +"cate_" +prefix
-    driver.find_element(cfg.get('exam','cate_mod_by'),cfg.get('exam','cate_mod_xpath')).click()
-    driver.find_element(cfg.get('exam','cate_addname_by'),cfg.get('exam','cate_addname')).clear()
-    driver.find_element(cfg.get('exam','cate_addname_by'),cfg.get('exam','cate_addname')).send_keys(cate_name)
-    driver.find_element(cfg.get('exam','cate_desname_by'),cfg.get('exam','cate_desname')).clear()
-    driver.find_element(cfg.get('exam','cate_desname_by'),cfg.get('exam','cate_desname')).send_keys(cate_detail)
+    driver.find_element(cfg.get('exam', 'cate_mod_by'),cfg.get('exam','cate_mod_xpath')).click()
+    driver.find_element(cfg.get('exam', 'cate_addname_by'),cfg.get('exam', 'cate_addname')).clear()
+    driver.find_element(cfg.get('exam', 'cate_addname_by'),cfg.get('exam', 'cate_addname')).send_keys(cate_name)
+    driver.find_element(cfg.get('exam', 'cate_desname_by'),cfg.get('exam', 'cate_desname')).clear()
+    driver.find_element(cfg.get('exam', 'cate_desname_by'),cfg.get('exam', 'cate_desname')).send_keys(cate_detail)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','cate_oknew_button_by'),cfg.get('exam','cate_oknew_button')).click()
+    driver.find_element(cfg.get('exam', 'cate_oknew_button_by'),cfg.get('exam', 'cate_oknew_button')).click()
     driver.implicitly_wait(10)
-    
+
 def delete_exam_cate(cfg, driver, base_url, org_name):
     driver.get("%sexam/" %(base_url))
     driver.implicitly_wait(10) 
     driver.find_element_by_link_text(u"类目管理").click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','cate_del_by'),cfg.get('exam','cate_del_xpath')).click()
+    driver.find_element(cfg.get('exam', 'cate_del_by'),cfg.get('exam', 'cate_del_xpath')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','cate_oknew_button_by'),cfg.get('exam','cate_oknew_button')).click()
+    driver.find_element(cfg.get('exam', 'cate_oknew_button_by'),cfg.get('exam', 'cate_oknew_button')).click()
     driver.implicitly_wait(10)
 
 
@@ -123,24 +121,24 @@ def create_exam_point(cfg, driver, base_url, org_name, point_name, point_detail,
     driver.get("%sexam/" %(base_url))
     driver.implicitly_wait(10) 
     driver.find_element_by_link_text(u"考点库").click()
-    driver.find_element(cfg.get('exam','point_addnewid_by'),cfg.get('exam','point_addnewid')).click()
-    driver.find_element(cfg.get('exam','point_addname_by'),cfg.get('exam','point_addname')).clear()
-    driver.find_element(cfg.get('exam','point_addname_by'),cfg.get('exam','point_addname')).send_keys(point_name)
-    driver.find_element(cfg.get('exam','point_desname_by'),cfg.get('exam','point_desname')).clear()
-    driver.find_element(cfg.get('exam','point_desname_by'),cfg.get('exam','point_desname')).send_keys(point_detail)
+    driver.find_element(cfg.get('exam', 'point_addnewid_by'),cfg.get('exam', 'point_addnewid')).click()
+    driver.find_element(cfg.get('exam', 'point_addname_by'),cfg.get('exam', 'point_addname')).clear()
+    driver.find_element(cfg.get('exam', 'point_addname_by'),cfg.get('exam', 'point_addname')).send_keys(point_name)
+    driver.find_element(cfg.get('exam', 'point_desname_by'),cfg.get('exam', 'point_desname')).clear()
+    driver.find_element(cfg.get('exam', 'point_desname_by'),cfg.get('exam', 'point_desname')).send_keys(point_detail)
     driver.implicitly_wait(10)
     driver.find_element_by_link_text("+").click()
     #driver.find_element_by_css_selector(cfg.get('exam','point_lesson_css')).click()
-    driver.find_element(cfg.get('exam','point_address_by'),cfg.get('exam','point_address_xpath1')).click()
+    driver.find_element(cfg.get('exam', 'point_address_by'),cfg.get('exam', 'point_address_xpath1')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_address_by'),cfg.get('exam','point_address_xpath2')).click()
+    driver.find_element(cfg.get('exam', 'point_address_by'),cfg.get('exam', 'point_address_xpath2')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_ok_lessbotton_by'),cfg.get('exam','point_ok_lessbotton')).click()
+    driver.find_element(cfg.get('exam', 'point_ok_lessbotton_by'),cfg.get('exam', 'point_ok_lessbotton')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_othergroom_by'),cfg.get('exam','point_othergroom')).clear()
-    driver.find_element(cfg.get('exam','point_othergroom_by'),cfg.get('exam','point_othergroom')).send_keys(other_groom)
+    driver.find_element(cfg.get('exam', 'point_othergroom_by'),cfg.get('exam', 'point_othergroom')).clear()
+    driver.find_element(cfg.get('exam', 'point_othergroom_by'),cfg.get('exam', 'point_othergroom')).send_keys(other_groom)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_okbotton_by'),cfg.get('exam','point_okbotton')).click()
+    driver.find_element(cfg.get('exam', 'point_okbotton_by'),cfg.get('exam', 'point_okbotton')).click()
     driver.implicitly_wait(10)
 
 def auto_create_exam_point(cfg, driver, base_url, org_name, point_num):
@@ -163,28 +161,27 @@ def modify_exam_point(cfg, driver, base_url, org_name):
     point_name = 'bianjikaodian'
     point_detail = 'kaodianmiaoshu'
     point_other = 'qitatuijian'
-    driver.find_element(cfg.get('exam','point_edit_by'),cfg.get('exam','point_edit')).click()
+    driver.find_element(cfg.get('exam', 'point_edit_by'),cfg.get('exam', 'point_edit')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_addname_by'),cfg.get('exam','point_addname')).clear()
-    driver.find_element(cfg.get('exam','point_addname_by'),cfg.get('exam','point_addname')).send_keys(point_name)
+    driver.find_element(cfg.get('exam', 'point_addname_by'),cfg.get('exam', 'point_addname')).clear()
+    driver.find_element(cfg.get('exam', 'point_addname_by'),cfg.get('exam', 'point_addname')).send_keys(point_name)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_desname_by'),cfg.get('exam','point_desname')).clear()
-    driver.find_element(cfg.get('exam','point_desname_by'),cfg.get('exam','point_desname')).send_keys(point_detail)
+    driver.find_element(cfg.get('exam', 'point_desname_by'),cfg.get('exam', 'point_desname')).clear()
+    driver.find_element(cfg.get('exam', 'point_desname_by'),cfg.get('exam', 'point_desname')).send_keys(point_detail)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_lesdel_botton_by'),cfg.get('exam','point_lesdel_botton')).click()
+    driver.find_element(cfg.get('exam', 'point_lesdel_botton_by'),cfg.get('exam','point_lesdel_botton')).click()
     driver.implicitly_wait(10)
-
     driver.find_element_by_link_text("+").click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_address_by'),cfg.get('exam','point_address_xpath1')).click()
-    driver.find_element(cfg.get('exam','point_address_by'),cfg.get('exam','point_address_xpath2')).click()
-    driver.find_element(cfg.get('exam','point_ok_lessbotton_by'),cfg.get('exam','point_ok_lessbotton')).click()
+    driver.find_element(cfg.get('exam', 'point_address_by'),cfg.get('exam', 'point_address_xpath1')).click()
+    driver.find_element(cfg.get('exam', 'point_address_by'),cfg.get('exam', 'point_address_xpath2')).click()
+    driver.find_element(cfg.get('exam', 'point_ok_lessbotton_by'),cfg.get('exam', 'point_ok_lessbotton')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_othergroom_by'),cfg.get('exam','point_othergroom')).click()
-    driver.find_element(cfg.get('exam','point_othergroom_by'),cfg.get('exam','point_othergroom')).clear()
-    driver.find_element(cfg.get('exam','point_othergroom_by'),cfg.get('exam','point_othergroom')).send_keys(point_other)
+    driver.find_element(cfg.get('exam', 'point_othergroom_by'),cfg.get('exam', 'point_othergroom')).click()
+    driver.find_element(cfg.get('exam', 'point_othergroom_by'),cfg.get('exam', 'point_othergroom')).clear()
+    driver.find_element(cfg.get('exam', 'point_othergroom_by'),cfg.get('exam', 'point_othergroom')).send_keys(point_other)
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_okbotton_by'),cfg.get('exam','point_okbotton')).click()
+    driver.find_element(cfg.get('exam', 'point_okbotton_by'),cfg.get('exam', 'point_okbotton')).click()
     driver.implicitly_wait(10)
 
 
@@ -193,7 +190,7 @@ def delete_exam_point(cfg, driver, base_url, org_name):
     driver.implicitly_wait(10) 
     driver.find_element_by_link_text(u"考点库").click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_delete_by'),cfg.get('exam','point_delete_xpath')).click()
+    driver.find_element(cfg.get('exam', 'point_delete_by'),cfg.get('exam', 'point_delete_xpath')).click()
     driver.implicitly_wait(10)
-    driver.find_element(cfg.get('exam','point_delokbot_by'),cfg.get('exam','point_delokbot_xpath')).click()
+    driver.find_element(cfg.get('exam', 'point_delokbot_by'),cfg.get('exam', 'point_delokbot_xpath')).click()
     driver.implicitly_wait(10)

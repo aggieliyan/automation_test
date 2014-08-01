90e53dae1ca168fcd09e1637ab1aefb850854fc0
yguo <yguo@ablesky.com>
2014-08-01 14:26:35 +0800

    修改一下格式

--- Test_regress/src/admin_management.py
+++ Test_regress/src/admin_management.py
@@ -8,7 +8,7 @@ Created on May 28, 2012
 import time, random
 
 def create_admin(cfg, driver, base_url, admin_name, admin_username, admin_psw, admin_email):
-    
+
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(10)
     driver.find_element_by_link_text(u"系统设置").click()
@@ -32,20 +32,20 @@ def create_admin(cfg, driver, base_url, admin_name, admin_username, admin_psw, a
     driver.implicitly_wait(10)
 
 def auto_create_admin(cfg, driver, base_url, org_name, adm_num):
-    
+
     prefix = chr(random.randint(97,122))+chr(random.randint(97,122))+chr(random.randint(97,122))
     admin_info = []  
     for i in range(adm_num):
-        
+
         admin_name = org_name[0] +"adm_" +prefix+str(i)
         admin_username = admin_name
         admin_psw ='123456aa'
         admin_email = admin_name+"@ablesky.com"
         create_admin(cfg,driver, base_url,admin_name,admin_username,admin_psw,admin_email)
         admin_info.append(admin_name)
-        
+   
     return admin_info
-    
+
 def delete_admin(cfg, driver, base_url, admin_num=1):
         
     driver.get(base_url + "myOffice.do")
@@ -61,16 +61,16 @@ def delete_admin(cfg, driver, base_url, admin_num=1):
     driver.implicitly_wait(10)   
     driver.find_element(cfg.get('org_manage','delete_ad_ok_xpath_by'),cfg.get('org_manage','delete_ad_ok_xpath')).click()
     driver.implicitly_wait(10)
-    
-    
+
+
+ 
 def modify_admin(cfg, driver, base_url):
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(10)
     driver.find_element_by_link_text(u"系统设置").click()
     driver.implicitly_wait(10)
     driver.find_element_by_link_text(u"网校管理员").click()
-    driver.implicitly_wait(10)
-    
+    driver.implicitly_wait(10) 
     prefix = chr(random.randint(97,122))+chr(random.randint(97,122))+chr(random.randint(97,122))
     admin_name = "adm_" +prefix
     #driver.find_element(cfg.get('org_index','org_manage_xpath_by'),cfg.get('org_index','org_manage_xpath')).click()
@@ -83,18 +83,3 @@ def modify_admin(cfg, driver, base_url):
     driver.execute_script("$(\"#editButton button\").eq(0).click()")
     driver.implicitly_wait(10)
     return admin_name
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        

91295578085ae71b97bb5a86009f0725bf390e3b
gaoyue001 <775694463@qq.com>
2014-08-01 17:34:56 +0800

    代码格式调整

--- Test_regress/src/cate_management.py
+++ Test_regress/src/cate_management.py
@@ -5,25 +5,23 @@ Created on Jun 2, 2012
 @author: yilulu
 '''
 import time
-
 #添加一级类目
-def add_cate(cfg,driver, base_url,org_name, cate_name=u'计算机'):
-
+def add_cate(cfg, driver, base_url, org_name, cate_name=u'计算机'):
     driver.get(base_url + "myOffice.do")
     time.sleep(2)
     driver.find_element_by_link_text(u"教学教务").click()    
     time.sleep(2)
     #driver.find_element_by_link_text(u"课程类目").click()
     #time.sleep(2)
-    driver.find_element(cfg.get('org_manage','add_topcate_by'),cfg.get('org_manage','add_topcate')).click()#新建一级类目
-    driver.find_element(cfg.get('org_manage','cate_addname_by'),cfg.get('org_manage','cate_addname')).send_keys(cate_name)
-    driver.find_element(cfg.get('org_manage','add_cate_ok_by'),cfg.get('org_manage','add_cate_ok')).click()
+    driver.find_element(cfg.get('org_manage', 'add_topcate_by'), \
+        cfg.get('org_manage', 'add_topcate')).click()#新建一级类目
+    driver.find_element(cfg.get('org_manage', 'cate_addname_by'), \
+        cfg.get('org_manage', 'cate_addname')).send_keys(cate_name)
+    driver.find_element(cfg.get('org_manage', 'add_cate_ok_by'), \
+        cfg.get('org_manage', 'add_cate_ok')).click()
     time.sleep(2)
- 
-
 #删除类目   
-def delete_cate(cfg,driver, base_url, org_name):
-    
+def delete_cate(cfg, driver, base_url, org_name):  
     driver.get(base_url + "myOffice.do")
     time.sleep(2)
     driver.find_element_by_link_text(u"教学教务").click() 
@@ -31,13 +29,12 @@ def delete_cate(cfg,driver, base_url, org_name):
     before_delete = driver.execute_script("return $(\".categTitle:last\").text()")#取最后一个类目的名称
     driver.execute_script("$(\".delete:last\").click()")
     time.sleep(2)
-    driver.find_element(cfg.get('org_manage','delete_cate_ok_by'),cfg.get('org_manage','delete_cate_ok')).click()
+    driver.find_element(cfg.get('org_manage', 'delete_cate_ok_by'), \
+        cfg.get('org_manage', 'delete_cate_ok')).click()
     time.sleep(2)
-    return before_delete #返回被删除的类目名
-    
+    return before_delete #返回被删除的类目名 
 #向类目中加入课程  
-def add_courese_to_cate(cfg,driver, base_url, org_name, cate_num = 0):
-    
+def add_courese_to_cate(cfg, driver, base_url, org_name, cate_num=0):
     driver.get(base_url + "myOffice.do")
     time.sleep(2)
     driver.find_element_by_link_text(u"教学教务").click()  
@@ -47,12 +44,13 @@ def add_courese_to_cate(cfg,driver, base_url, org_name, cate_num = 0):
     time.sleep(3)
     driver.find_element_by_link_text(u"向类目添加知识资料").click()
     time.sleep(4)
-    driver.find_element(cfg.get('org_manage','course_add_1_by'),cfg.get('org_manage','course_add_1')).click()
+    driver.find_element(cfg.get('org_manage', 'course_add_1_by'), \
+        cfg.get('org_manage', 'course_add_1')).click()
     add_course_name = driver.execute_script("return $(\"input[name='win_groupCheck']:eq(0)\").parent().parent().next().children().text()")
     add_course_name = add_course_name.strip()
     #add_course_name = add_course_name.encode('UTF-8','ignore')
-    driver.find_element(cfg.get('org_manage','course_add_ok_by'),cfg.get('org_manage','course_add_ok')).click()
+    driver.find_element(cfg.get('org_manage', 'course_add_ok_by'), \
+        cfg.get('org_manage', 'course_add_ok')).click()
     time.sleep(1) 
     return add_course_name
-
     

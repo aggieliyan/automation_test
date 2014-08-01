718ae85df420bd462da2090d0ec30071707a23f3
gaoyue001 <775694463@qq.com>
2014-08-01 18:12:15 +0800

    代码格式调整

--- Test_regress/src/exam_user_management.py
+++ Test_regress/src/exam_user_management.py
@@ -11,13 +11,14 @@ def buy_paper(cfg, driver, paper_url):
     #paper_url = "http://www.gamma.ablesky.com/examRedirect.do?action=viewExamPaperInfo&examPaperId=6129"
     driver.get(paper_url)
     time.sleep(1)
-    driver.find_element(cfg.get('exam', 'buy_paper_by'), cfg.get('exam', 'buy_paper')).click()
+    driver.find_element(cfg.get('exam', 'buy_paper_by'), \
+        cfg.get('exam', 'buy_paper')).click()
     h = driver.window_handles
     driver.switch_to_window(h[-1])
     driver.implicitly_wait(2)
-    driver.find_element(cfg.get('org_index', 'pay_ok_by'), cfg.get('org_index', 'pay_ok')).click()
+    driver.find_element(cfg.get('org_index', 'pay_ok_by'), \
+        cfg.get('org_index', 'pay_ok')).click()
     time.sleep(5)
-
 #学员考试
 def exam_user(cfg, driver, base_url, operation, blank_pager, question_answer):
     time.sleep(2)
@@ -36,20 +37,23 @@ def exam_user(cfg, driver, base_url, operation, blank_pager, question_answer):
     if blank_pager == 0:
         #单选 多选
         if question_title == u"单选题" or question_title == u"多选题":
-            try:   
-                driver.find_element(cfg.get('exam','exam_selectque_by'),cfg.get('exam','exam_selectque')).click()
+            try:
+                driver.find_element(cfg.get('exam', 'exam_selectque_by'), \
+                    cfg.get('exam', 'exam_selectque')).click()
             except:
                 None     
         #是非题
         elif question_title == u"是非题":
             try: 
-                driver.find_element(cfg.get('exam','exam_yesnoque_by'),cfg.get('exam','exam_yesnoque')).click()   
+                driver.find_element(cfg.get('exam', 'exam_yesnoque_by'), \
+                    cfg.get('exam', 'exam_yesnoque')).click()   
             except:
                 None      
         #填空题
         elif question_title == u"填空题":
             try: 
-                driver.find_element(cfg.get('exam','exam_blankque_by'),cfg.get('exam','exam_blankque')).send_keys(question_answer)   
+                driver.find_element(cfg.get('exam', 'exam_blankque_by'), \
+                    cfg.get('exam', 'exam_blankque')).send_keys(question_answer)   
             except:
                 None  
         #问答题  
@@ -64,7 +68,8 @@ def exam_user(cfg, driver, base_url, operation, blank_pager, question_answer):
         #完形填空题
         elif question_title == u"完形填空题":
             try:
-                driver.find_element(cfg.get('exam','exam_clozeque_by'),cfg.get('exam','exam_clozeque')).click()
+                driver.find_element(cfg.get('exam', 'exam_clozeque_by'), \
+                    cfg.get('exam', 'exam_clozeque')).click()
             except:
                 None 
         #综合题
@@ -72,25 +77,31 @@ def exam_user(cfg, driver, base_url, operation, blank_pager, question_answer):
             #第一个是单选 or 多选
             time.sleep(2)
             try:
-                if driver.find_element(cfg.get('exam','exam_all_selectque_by'),cfg.get('exam','exam_all_selectque')):
-                    driver.find_element(cfg.get('exam','exam_all_selectque_by'),cfg.get('exam','exam_all_selectque')).click()
+                if driver.find_element(cfg.get('exam', 'exam_all_selectque_by'), \
+                        cfg.get('exam', 'exam_all_selectque')):
+                    driver.find_element(cfg.get('exam', 'exam_all_selectque_by'), \
+                        cfg.get('exam', 'exam_all_selectque')).click()
             except:
                 None
             #是非
             try:
-                if driver.find_element(cfg.get('exam','exam_all_yesque_by'),cfg.get('exam','exam_all_yesque')):
-                    driver.find_element(cfg.get('exam','exam_all_yesque_by'),cfg.get('exam','exam_all_yesque')).click()
+                if driver.find_element(cfg.get('exam', 'exam_all_yesque_by'), \
+                        cfg.get('exam', 'exam_all_yesque')):
+                    driver.find_element(cfg.get('exam', 'exam_all_yesque_by'), \
+                        cfg.get('exam', 'exam_all_yesque')).click()
             except:
                 None
             #填空
             try:
-                if driver.find_element(cfg.get('exam','exam_all_blankque_by'),cfg.get('exam','exam_all_blankque')):
-                    driver.find_element(cfg.get('exam','exam_all_blankque_by'),cfg.get('exam','exam_all_blankque')).send_keys(question_answer)
+                if driver.find_element(cfg.get('exam', 'exam_all_blankque_by'), \
+                        cfg.get('exam', 'exam_all_blankque')):
+                    driver.find_element(cfg.get('exam', 'exam_all_blankque_by'), \
+                        cfg.get('exam', 'exam_all_blankque')).send_keys(question_answer)
             except:  
                 None
             #问答
             try:
-                if driver.find_element(cfg.get('exam','exam_all_askque_by'),cfg.get('exam','exam_all_askque')):
+                if driver.find_element(cfg.get('exam', 'exam_all_askque_by'), cfg.get('exam', 'exam_all_askque')):
                     iframe_id = driver.execute_script("return $('#J_examWrapper iframe:eq(0)').attr('id')")
                     driver.execute_script("var element=window.document.getElementById('" + iframe_id + "');\
                     idocument=element.contentDocument;element=idocument.getElementById('tinymce');\
@@ -100,13 +111,17 @@ def exam_user(cfg, driver, base_url, operation, blank_pager, question_answer):
         ###综合题结束
         time.sleep(exam_time * 60 + 2)
         try:   
-            driver.find_element(cfg.get('exam','exam_submit_by'),cfg.get('exam','exam_submit')).click()#提交
+            driver.find_element(cfg.get('exam', 'exam_submit_by'), \
+                cfg.get('exam', 'exam_submit')).click()#提交
             time.sleep(2)
-            driver.find_element(cfg.get('exam','exam_continue_by'),cfg.get('exam','exam_continue')).click()#弹窗-继续考试
+            driver.find_element(cfg.get('exam', 'exam_continue_by'), \
+                cfg.get('exam', 'exam_continue')).click()#弹窗-继续考试
             time.sleep(2)
-            driver.find_element(cfg.get('exam','exam_submit_by'),cfg.get('exam','exam_submit')).click()#提交
+            driver.find_element(cfg.get('exam', 'exam_submit_by'), \
+                cfg.get('exam', 'exam_submit')).click()#提交
             time.sleep(2)
-            driver.find_element(cfg.get('exam','window_submit_by'),cfg.get('exam','window_submit')).click()#弹窗-提交
+            driver.find_element(cfg.get('exam', 'window_submit_by'), \
+                cfg.get('exam', 'window_submit')).click()#弹窗-提交
             time.sleep(2)  
         except:
              None
@@ -114,13 +129,17 @@ def exam_user(cfg, driver, base_url, operation, blank_pager, question_answer):
     else:
         time.sleep(exam_time * 60 + 2)
         try:   
-            driver.find_element(cfg.get('exam','exam_submit_by'),cfg.get('exam','exam_submit')).click()#提交
+            driver.find_element(cfg.get('exam', 'exam_submit_by'), \
+                cfg.get('exam', 'exam_submit')).click()#提交
             time.sleep(2)
-            driver.find_element(cfg.get('exam','exam_continue_by'),cfg.get('exam','exam_continue')).click()#弹窗-继续考试
+            driver.find_element(cfg.get('exam', 'exam_continue_by'), \
+                cfg.get('exam', 'exam_continue')).click()#弹窗-继续考试
             time.sleep(2)
-            driver.find_element(cfg.get('exam','exam_submit_by'),cfg.get('exam','exam_submit')).click()#提交
+            driver.find_element(cfg.get('exam', 'exam_submit_by'), \
+                cfg.get('exam', 'exam_submit')).click()#提交
             time.sleep(2)
-            driver.find_element(cfg.get('exam','window_submit_by'),cfg.get('exam','window_submit')).click()#弹窗-提交
+            driver.find_element(cfg.get('exam', 'window_submit_by'), \
+                cfg.get('exam', 'window_submit')).click()#弹窗-提交
             time.sleep(2)  
         except:
              None

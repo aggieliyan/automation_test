25e6227e90d57610501ad3d2a2d4bad5222b9da8
wli <wli@ablesky.com>
2014-08-04 10:37:35 +0800

    删掉不用的方法

--- Test_regress/src/as_test_regress.py
+++ Test_regress/src/as_test_regress.py
@@ -1208,14 +1208,6 @@ class Test(unittest.TestCase):
         #考试系统部分
 
         #self.login_from_index()
-        #self.exam_question_danxuan()
-        #self.exam_question_duoxuan()
-        #self.exam_question_shifei()
-        #self.exam_question_tiankong()
-        #self.exam_question_wenda()
-        #self.exam_question_wanxing()
-        #self.exam_question_zonghe()
-        self.login_from_index()
         #self.exam_onequestion()
         #self.exam_questions()
         #self.import_questions()

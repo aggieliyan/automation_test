bb30cc644aadbbc5818fdac558d5efd4d477f05c
hjliu <hjliu@ablesky.com>
2014-08-06 13:48:09 +0800

    格式修改

--- Test_regress/src/student_management.py
+++ Test_regress/src/student_management.py
@@ -4,7 +4,7 @@ Created on Jun 2, 2012
 
 @author: yilulu
 '''
-import time,random
+import time, random
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -12,10 +12,10 @@ from selenium.webdriver.common.by import By
 def is_element_present(driver, how, what):
     try: driver.find_element(by=how, value=what)
     except NoSuchElementException, e: return False
-    return True 
+    return True
 
 def import_one_student(cfg, driver, base_url, org_name, stu_name):
-    
+
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(2)
     driver.find_element_by_link_text(u"学员/员工").click()
@@ -33,9 +33,9 @@ def import_one_student(cfg, driver, base_url, org_name, stu_name):
     driver.find_element(cfg.get('org_manage', "stu_import_btn_by"), \
     	cfg.get('org_manage', "stu_import_btn")).click()
     driver.implicitly_wait(3)
-    
+
 def import_multi_student(cfg, driver, base_url, org_name, stu_txt):
-    
+
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(2)
     driver.find_element_by_link_text(u"学员/员工").click()
@@ -55,12 +55,11 @@ def import_multi_student(cfg, driver, base_url, org_name, stu_txt):
     driver.find_element(cfg.get('org_manage', "stu_file_ok_by"), \
     	cfg.get('org_manage', "stu_file_ok")).click()
     count = 0
-    while is_element_present(driver,By.LINK_TEXT, u"继续批量导入学员")!= True or count <= 30:
+    while is_element_present(driver, By.LINK_TEXT, u"继续批量导入学员") != True or count <= 30:
         time.sleep(3)
         count += 3
 
 def create_student(cfg, driver, base_url, org_name, stu_txt):
-    
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(2)
     driver.find_element_by_link_text(u"学员/员工").click()
@@ -82,30 +81,30 @@ def create_student(cfg, driver, base_url, org_name, stu_txt):
     	cfg.get('org_manage', "stu_file_ok")).click()
     driver.implicitly_wait(2)
     count = 0
-    while is_element_present(driver,By.LINK_TEXT, u"继续批量创建学员")!= True: #or count >= 30:
+    while is_element_present(driver, By.LINK_TEXT, u"继续批量创建学员") != True: #or count >= 30:
         driver.implicitly_wait(3)
         count += 3
 
-def auto_create_student(cfg,driver, base_url,org_name, stu_num):#自动生成用户名文件创建学员
+def auto_create_student(cfg, driver, base_url, org_name, stu_num):
+    #自动生成用户名文件创建学员
     #stu_num为需要创建学员的数量
-    prefix = chr(random.randint(97,122)) + chr(random.randint(97,122)) + chr(random.randint(97,122))
+    prefix = chr(random.randint(97, 122)) + chr(random.randint(97, 122)) \
+     + chr(random.randint(97, 122))
     stu_txt = r"C:\create_stu.txt"
-    stu_file = open(stu_txt,'w')
+    stu_file = open(stu_txt, 'w')
     for i in range(stu_num):
-        stu_name = 'stu_' + prefix + str(i) 
+        stu_name = 'stu_' + prefix + str(i)
         stu_psw = '1234aa'
         if i == 0:
             stu_file.writelines(" \n")
         stu_file.writelines(stu_name + " " + stu_psw + "\n")
-        
     stu_file.close()
     create_student(cfg, driver, base_url, org_name, stu_txt)
 
-def open_course_for_one(cfg, driver, base_url, org_name, stu_num = 1):
-    
+def open_course_for_one(cfg, driver, base_url, org_name, stu_num=1):
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(2)
-    driver.find_element_by_link_text(u"学员/员工").click()  
+    driver.find_element_by_link_text(u"学员/员工").click()
     try:
         driver.implicitly_wait(2)
         driver.find_element(cfg.get('org_manage', "stu_close_by"), \
@@ -115,7 +114,8 @@ def open_course_for_one(cfg, driver, base_url, org_name, stu_num = 1):
     driver.implicitly_wait(2)
     driver.find_element_by_link_text(u"学员管理").click()
     time.sleep(5)
-    driver.find_element_by_xpath("//div["+str(stu_num)+"]/table/tbody/tr/td[2]/div/p/a").click() #开通知识资料
+    driver.find_element_by_xpath("//div["+str(stu_num)+"]/table/tbody/tr/td[2]/div/p/a").click()
+    #开通知识资料
     time.sleep(4)
     driver.find_element(cfg.get('org_manage', "open_cate_by"), \
     cfg.get('org_manage', "open_cate")).click()#未归类内容，展开资料
@@ -126,10 +126,10 @@ def open_course_for_one(cfg, driver, base_url, org_name, stu_num = 1):
     cfg.get('org_manage', "open_ok")).click()#确认开通
     time.sleep(2)
     driver.find_element(cfg.get('org_manage', "open_popup_by"), \
-    cfg.get('org_manage', "open_popup")).click()#弹出框中确认      
+    cfg.get('org_manage', "open_popup")).click()#弹出框中确认
     time.sleep(2)
 
-def open_course_for_multi(cfg, driver, base_url, org_name):    
+def open_course_for_multi(cfg, driver, base_url, org_name):
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(2)
     driver.find_element_by_link_text(u"学员/员工").click()
@@ -167,10 +167,9 @@ def open_course_for_multi(cfg, driver, base_url, org_name):
     driver.find_element(cfg.get('org_manage', "open_popup_by"), \
     cfg.get('org_manage', "open_popup")).click()#弹出框中确认
     time.sleep(2)
-       
-#购买开通授权数 bnum为购买的数量    
+
+#购买开通授权数 bnum为购买的数量
 def buy_open_num(cfg, driver, base_url, org_name, bnum):
-    
     driver.get(base_url + "myOffice.do")
     driver.implicitly_wait(3)
     driver.find_element_by_link_text("在线购买授权数").click()
@@ -184,7 +183,7 @@ def buy_open_num(cfg, driver, base_url, org_name, bnum):
     driver.implicitly_wait(3)
     driver.find_element_by_link_text(u"继续购买授权").click()
     driver.implicitly_wait(3)
-    
+
 #管理播放授权数
 def manage_course_num(cfg, driver, base_url):
     driver.get(base_url + "myOffice.do")
@@ -201,18 +200,22 @@ def manage_course_num(cfg, driver, base_url):
     time.sleep(5)
     driver.find_element_by_link_text("管理播放授权数").click()
     driver.implicitly_wait(2)
-    driver.find_element(cfg.get('manage_course_num', "manage_coursenum_opencouse_by"), \
+    driver.find_element(cfg.get('manage_course_num', \
+        "manage_coursenum_opencouse_by"), \
     	cfg.get('manage_course_num', "manage_coursenum_opencouse")).click()
     driver.implicitly_wait(2)
-    driver.find_element(cfg.get('manage_course_num', "manage_coursenum_opennum_by"), \
+    driver.find_element(cfg.get('manage_course_num', \
+        "manage_coursenum_opennum_by"), \
     	cfg.get('manage_course_num', "manage_coursenum_opennum")).click()
     driver.implicitly_wait(2)
     driver.find_element_by_link_text("修改剩余播放次数").click()
     driver.implicitly_wait(2)
-    driver.find_element(cfg.get('manage_course_num', 'manage_coursenum_change_by'), \
+    driver.find_element(cfg.get('manage_course_num', \
+        'manage_coursenum_change_by'), \
     	cfg.get('manage_course_num', 'manage_coursenum_change')).clear()
-    driver.implicitly_wait(2)    
-    driver.find_element(cfg.get('manage_course_num', 'manage_coursenum_change_by'), \
+    driver.implicitly_wait(2)
+    driver.find_element(cfg.get('manage_course_num', \
+        'manage_coursenum_change_by'), \
     	cfg.get('manage_course_num', 'manage_coursenum_change')).send_keys("1")
     driver.implicitly_wait(2)
     driver.find_element_by_link_text("保存").click()

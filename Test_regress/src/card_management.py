48ee9792dd05230a31dd64f98abdf59b083273bc
gaoyue001 <775694463@qq.com>
2014-08-08 13:02:16 +0800

    处理异常

--- Test_regress/src/card_management.py
+++ Test_regress/src/card_management.py
@@ -182,7 +182,10 @@ def get_academy_catename(cfg, driver, base_url):
     time.sleep(2)
     driver.get(base_url + "qqhru")
     time.sleep(2)
-    driver.find_element_by_link_text(u"跳过").click()#点击跳过
+    try:
+        driver.find_element_by_link_text(u"跳过").click()#点击跳过
+    except:
+        pass
     time.sleep(2)
     driver.find_element(cfg.get('org_manage', 'exam_selectcourse_by'), \
         cfg.get('org_manage', 'exam_selectcourse')).click()#点击选课

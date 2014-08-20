7a1bacb048a9841c946e0c515c604c1aefb857e1
yilu282 <1049924595@qq.com>
2014-08-20 16:11:51 +0800

    删掉多余的行

--- Test_regress/src/student_management.py
+++ Test_regress/src/student_management.py
@@ -6,7 +6,6 @@ Created on Jun 2, 2012
 '''
 import time, random
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 def is_element_present(driver, how, what):

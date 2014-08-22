e4eede8a5a85bf3cac9b3c08a2cc70b448473cf3
gaoyue001 <775694463@qq.com>
2014-08-22 17:09:45 +0800

    更新休眠时间

--- Test_regress/src/card_management.py
+++ Test_regress/src/card_management.py
@@ -199,9 +199,9 @@ def get_academy_catename(cfg, driver, base_url, academy):
         driver.find_element(cfg.get('org_manage', 'exam_select_close_by'), \
             cfg.get('org_manage', 'exam_select_close')).click()#关闭窗口   
     except:
-        time.sleep(1)
+        driver.implicitly_wait(30)
         driver.find_element_by_link_text(u"跳过").click()#点击跳过
-        time.sleep(2)
+        driver.implicitly_wait(30)
         driver.find_element(cfg.get('org_manage', 'exam_selectcourse_by'), \
             cfg.get('org_manage', 'exam_selectcourse')).click()#点击选课
     time.sleep(2)

8e1bed8e4cff67b7d8e383dcd9c4b06ee6311c61
yilu282 <1049924595@qq.com>
2014-08-22 15:49:15 +0800

    课程管理页面权限检查

--- Test_regress/src/test_authority.py
+++ Test_regress/src/test_authority.py
@@ -6,23 +6,60 @@ import time
 from selenium import webdriver
 
 import login
+import new_course_management
 
 def execute_func(func_name):
 	func_name()
-
+#课程类目
 def course_cate():
 	driver.find_element("id", "J_genTopCateg").click()#新建一级类目
 	driver.refresh()
 
+#课程管理
 def course_manage():
+	current_url = driver.current_url
+	time.sleep(3)
 	try:
+		driver.find_element_by_link_text(u"获取视频链接").click()
+		time.sleep(1)
+	except:
+		print u"没有课程读权限"
+
+	try:		
+		#编辑
+		driver.get(current_url)
 		driver.find_element_by_link_text(u"编辑").click()
 		driver.execute_script("$('submit').click()")
-		driver.get("%smyOffice.do" %(base_url))
-		driver.find_element_by_link_text(u"教学教务").click()
+		try:
+			driver.find_element("id", "J_complete").click()
+		except:
+			pass
+		time.sleep(1)
+		#发布相似课程
+		driver.get(current_url)
+		time.sleep(2)
+		driver.find_element_by_link_text(u"发布相似课程").click()
+		time.sleep(1)
+		#编辑三分屏章节
+		driver.get(current_url)
+		alert = driver.switch_to_alert()
+		alert.accept()
+		time.sleep(3)
+		driver.find_element_by_link_text(u"编辑三分屏章节").click()
 		time.sleep(1)
 	except:
 		print u"没有课程编辑权限"
+
+	try:
+		#删除
+		driver.get(current_url)
+		time.sleep(2)
+		driver.find_element_by_link_text(u"删除").click()
+		time.sleep(1)
+		driver.find_element("xpath", "//button").click()
+		time.sleep(1)
+	except:
+		print u"没有课程删除权限"
 
 
 def course():
@@ -30,12 +67,12 @@ def course():
 	#course_func = [u'课程类目', u'课程管理', u'课件存储空间', u'视频外链管理', u'播放高级设置']
 	course_func = {u"课程类目":course_cate, u"课程管理":course_manage,}
 	try:
-		driver.implicitly_wait(10)
+		time.sleep(2)
 		driver.find_element_by_link_text(u"教学教务").click()
-		time.sleep(5)
+		time.sleep(1)
 		for item in course_func.keys():
 			try:
-				driver.implicitly_wait(10)
+				driver.implicitly_wait(5)
 				driver.find_element_by_link_text(item).click()
 				time.sleep(1)
 				execute_func(course_func[item])
@@ -55,12 +92,12 @@ def admin_athority_check():
 	global base_url
 	global cfg 
 	global driver
-	base_url = "http://www.ablesky.com/"
+	base_url = "http://www.gamma.ablesky.com/"
 	cfg_file = 'config.ini'
 	cfg = ConfigParser.RawConfigParser()
 	cfg.read(cfg_file)
-	user_name = ""
-	user_psw = ""
+	user_name = "sadm001"
+	user_psw = "1234"
 
 	chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 	os.environ["webdriver.chrome.driver"] = chromedriver

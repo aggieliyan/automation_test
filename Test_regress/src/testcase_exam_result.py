c9d8e7b16264006a5ac9957cb2db2338db967292
ygao <ygao@ablesky.com>
2015-07-27 11:15:40 +0800

    考试系统调用chrome方法

--- Test_regress/src/testcase_exam_result.py
+++ Test_regress/src/testcase_exam_result.py
@@ -12,14 +12,17 @@ class ExamResultTest(unittest.TestCase):
 
 
     def setUp(self):
+        
         self.cfg_file = 'config.ini'
         self.cfg = ConfigParser.RawConfigParser()
         self.cfg.read(self.cfg_file)
         self.browser = self.cfg.get("env_para", "browser")
+        self.org_name = self.cfg.get("env_para", "org_name")
+        self.org_password = self.cfg.get("env_para", "org_password")
         self.user_name = self.cfg.get("env_para", "user_name")
+        self.user_password = self.cfg.get("env_para", "user_password")
         self.base_url = self.cfg.get("env_para", "base_url")
         self.paper_name = self.cfg.get("env_para", "paper_name")
-
 
         if os.path.exists("C:\\test_rs_pic") != True:
                 os.system("mkdir C:\\test_rs_pic")
@@ -29,9 +32,10 @@ class ExamResultTest(unittest.TestCase):
         elif self.browser == 'firefox':
             self.driver = webdriver.Firefox()
         elif self.browser == 'Chrome':
-            chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
-            os.environ["webdriver.chrome.driver"] = chromedriver
-            self.driver = webdriver.Chrome(chromedriver)
+            # chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+            # os.environ["webdriver.chrome.driver"] = chromedriver
+            # self.driver = webdriver.Chrome(chromedriver)
+            self.driver = webdriver.Chrome()
         elif self.browser == "Html":
             self.driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.HTMLUNIT.copy())
         else:
@@ -43,11 +47,8 @@ class ExamResultTest(unittest.TestCase):
 
         cookie1 = self.cfg.get('env_para', 'cookie1')
         if(cookie1 == 'no'):
-            self.org_name = self.cfg.get("env_para", "org_name")
-            self.org_password = self.cfg.get("env_para", "org_password")
             login.login_by_logindo(self.cfg, self.driver, self.base_url, self.org_name, self.org_password)
-            self.cfg.set("env_para", "cookie1", \
-                str(self.driver.get_cookie('ASUSS')['value']))
+            self.cfg.set("env_para", "cookie1", str(self.driver.get_cookie('ASUSS')['value']))
             self.cfg.write(open(self.cfg_file, "w"))
            
             #本来还有一个叫RM的cookie，但是值都是rm不变所以不取了

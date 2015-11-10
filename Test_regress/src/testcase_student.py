4e6741fb42089fa1232b440c08de165729c4aef4
ygao <ygao@ablesky.com>
2015-11-10 18:03:25 +0800

    收费课程

--- Test_regress/src/testcase_student.py
+++ Test_regress/src/testcase_student.py
@@ -73,7 +73,7 @@ class StudentTest(unittest.TestCase):
        ba.save_screenshot()
        
     def test_buy_course_use_card(self):
-        course_href = self.cfg.get("env_para", "course_href2")
+        course_href = self.cfg.get("env_para", "course_href1")
         if course_href != '0':
             user_management.buy_course_usecard(self.cfg, self.driver, self.base_url, course_href)
         else:

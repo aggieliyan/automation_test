f143f24ad94380e6f6fdeb6340b292e99af37053
liyan0424 <liyanceshi001@126.com>
2016-05-26 10:05:20 +0800

    修改老师管理流程

--- Test_regress/src/PO/org_teacher_page.py
+++ Test_regress/src/PO/org_teacher_page.py
@@ -21,11 +21,16 @@ class OrgteacherManagePage(base.Base):
 		m.open()
 		m.click_student()
 		m.click_teacher()
+		time.sleep(20)        
 		
 	def click_createacher(self):
 		self.dr.find_element(self.cfg.get('org_teacher', 'create_by'), \
 		self.cfg.get('org_teacher', 'create')).click()
-		
+
+	def click_manage(self):
+		self.dr.find_element(self.cfg.get('org_teacher', 'manage_by'), \
+		self.cfg.get('org_teacher', 'manage')).click()
+			        		
 	def click_editeacher(self):
 		self.dr.find_element_by_link_text(u"编辑").click()
 		
@@ -54,6 +59,22 @@ class OrgteacherCreatePage(base.Base):
 	def input_recommend(self, commend):
 		self.dr.find_element(self.cfg.get('org_teacher', 'recommend_by'), \
 		self.cfg.get('org_teacher', 'recommend')).send_keys(commend)
+		
+	def select_true(self):
+		self.dr.find_element_by_css_selector\
+		(".select-radio input[data-type='show']").click()
+		
+	def input_account(self, account):
+		self.dr.find_element(self.cfg.get('org_teacher', 'account_by'), \
+		self.cfg.get('org_teacher', 'account')).send_keys(account)
+		
+	def input_password(self, password):
+		self.dr.find_element(self.cfg.get('org_teacher', 'password_by'), \
+		self.cfg.get('org_teacher', 'password')).send_keys(password)
+		
+	def input_mail(self, mail):
+		self.dr.find_element(self.cfg.get('org_teacher', 'mail_by'), \
+		self.cfg.get('org_teacher', 'mail')).send_keys(mail)	
 	
 	def click_publish(self):
 		self.dr.find_element(self.cfg.get('org_teacher', 'publish_by'), \

2ec892dd1616a0134f71502f5d6bb96dcb89224d
liyan0424 <liyanceshi001@126.com>
2016-12-09 14:42:42 +0800

    修复2级导航更改导致元素定位不到问题

--- Test_regress/src/PO/myoffice_page.py
+++ Test_regress/src/PO/myoffice_page.py
@@ -109,7 +109,7 @@ class MyOfficePage(base.Base):
 	#点击管理/卡组
 	def click_org_mancardgroup(self):
 		time.sleep(2)
-		self.dr.find_element_by_link_text(u"管理卡组").click()
+		self.dr.find_element_by_link_text(u"新建/管理卡").click()
 
 	#去在线购买授权页面
 	def click_num_record(self):
